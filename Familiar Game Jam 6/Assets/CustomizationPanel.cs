eed8895290bfd098982d8def58d6cd7d3f85bdd1
LuisPalomo <luispalomosiv@gmail.com>
2016-04-02 15:39:32 +0200

    Main menu and textures

---
+++ Familiar Game Jam 6/Assets/CustomizationPanel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public enum Gender { Male, Female };
+
+public class CustomizationPanel : MonoBehaviour
+{
+    public GameObject loadingIndicator;
+
+    public bool IsMale { get; set; }
+    public float Height { get; set; }
+    public float Weight { get; set; }
+    public int ColorIndex { get; set; }
+
+    public void SetGender(bool isMale)
+    {
+        this.IsMale = isMale;
+        this.ShowLoading();
+    }
+
+    public void SetHeight(float height)
+    {
+        this.Height = height;
+        this.ShowLoading();
+    }
+
+    public void SetWeight(float weight)
+    {
+        this.Weight = weight;
+        this.ShowLoading();
+    }
+
+    public void SetColorIndex(int colorIndex)
+    {
+        this.ColorIndex = ColorIndex;
+        this.ShowLoading();
+    }
+
+    private void ShowLoading()
+    {
+        this.loadingIndicator.SetActive(false);
+        this.StopAllCoroutines();
+        this.StartCoroutine(this.ShowIndicator());
+    }
+
+    private IEnumerator ShowIndicator()
+    {
+        this.loadingIndicator.SetActive(true);
+        yield return new WaitForSeconds(UnityEngine.Random.Range(0.25f, 1.0f));
+        this.loadingIndicator.SetActive(false);
+    }
+
+}
---
+++ Familiar Game Jam 6/Assets/GetSliderValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetSliderValue : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+
+    private void Update()
+    {
+        this.text.text = string.Format("{0:0.0}", slider.value);
+    }
+
+}
