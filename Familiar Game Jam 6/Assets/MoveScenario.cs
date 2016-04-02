074d211825429b6576dec504481e8bf29b4b3cd0
LuisPalomo <luispalomosiv@gmail.com>
2016-04-02 20:34:46 +0200

    New changes

---
+++ Familiar Game Jam 6/Assets/MoveScenario.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Extensions.System.Colections;
+
+public class MoveScenario : MonoBehaviour {
+
+    public float speed = 0.3f;
+    public List<GameObject> piezas;
+
+	// Use this for initialization
+	void Start () {
+
+        //GameObject piece = (GameObject)Instantiate(piezas.RandomItem(), transform.position + new Vector3(3f, -0.4f), Quaternion.identity);
+        //piece.transform.SetParent(this.transform);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "ScenarioGen")
+        {
+            GameObject piece = (GameObject)Instantiate(piezas.RandomItem(), transform.position + new Vector3(3f, -0.4f), Quaternion.identity);
+            piece.transform.SetParent(this.transform);
+        }
+    }
+}
---
+++ Familiar Game Jam 6/Assets/Scripts/CursorAutoScaler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorAutoScaler : MonoBehaviour
+{
+    public float sizeFactor = 5.0f;
+
+    private int currentHeight;
+    private int currentWidth;
+
+    private void Awake()
+    {
+        this.currentHeight = Screen.height;
+        this.currentHeight = Screen.width;
+    }
+
+    private void Update()
+    {
+        if (this.currentHeight != Screen.height)
+        {
+            this.currentHeight = Screen.height;
+            this.transform.localScale = (Vector2.one * this.sizeFactor) / this.currentHeight;
+        }
+    }
+
+}
