3fbd38a666d6cf97026d2ee626c93de8b6446ec0
LuisPalomo <luispalomosiv@gmail.com>
2016-04-03 12:03:52 +0200

    Life Coin Counters

---
+++ Familiar Game Jam 6/Assets/CoinCounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = "" + GameManager.Instance.coins;
+    }
+}
---
+++ Familiar Game Jam 6/Assets/LifeCounter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LifeCounter : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = "x" + GameManager.Instance.lives;
+	}
+}
