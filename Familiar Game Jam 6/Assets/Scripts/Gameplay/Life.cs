fc1e2d9e23d67644acced3b1ff837329a9fd4e48
NandoFreak <ferran_rofes@hotmail.com>
2016-04-03 14:06:04 +0200

    huevo - life

---
+++ Familiar Game Jam 6/Assets/Scripts/Gameplay/Life.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Life : MonoBehaviour {
+
+	public float velocidad;
+
+
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Translate (new Vector2 (velocidad, 0),Space.World);
+	}
+} 
